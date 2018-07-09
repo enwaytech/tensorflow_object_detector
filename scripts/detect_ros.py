24513295c5c3daf662d2297b5efa09b422d7a1fe
Thanuja Ambegoda <thanuja@enway.ai>
2018-07-09 15:37:15 +0200

    removed unused method and debug messages

--- scripts/detect_ros.py
+++ scripts/detect_ros.py
@@ -87,57 +87,19 @@ with detection_graph.as_default():
       def generate_im_patch(self, img_in, patch_size, patch_stride):
         self.rows = img_in.shape[0]
         self.cols = img_in.shape[1]
-        print("img_in rows = %d, cols = %d", (self.rows, self.cols))
+        
         # get the bottom half of the image
         img = img_in[(self.rows / 2 + 1) : self.rows, :]
         img_cv = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         save_name = '/home/thanuja/test/outputs/in.png'
-        print("output saved!**************")
+        print("input saved!**************")
         cv2.imwrite(save_name, img_cv)
-        print("sub image img.shape = ")
-        print(img.shape)
         patch_id = 0
         for i in range(0, img.shape[0] - patch_size, patch_stride):
           for j in range(0, img.shape[1] - patch_size, patch_stride):
             patch_id = patch_id + 1
             im_patch = img[i : i + patch_size, j : j + patch_size, ...]
             yield im_patch, i, j
-
-      def results_aggregator(self, boxes, scores, classes, num_detections, \
-                    boxes_all_t, scores_all_t, classes_all_t, num_detections_all, \
-                    y0, x0):
-        # box_coords = ymin, xmin, ymax, xmax
-        num_predictions = len(scores)
-        for i in range(0, num_predictions):
-          ymin = (boxes[i][0] * patch_size + y0 + self.rows / 2) / self.rows
-          ymax = (boxes[i][2] * patch_size + y0 + self.rows / 2) / self.rows
-          xmin = (boxes[i][1] * patch_size + x0) / self.cols
-          xmax = (boxes[i][3] * patch_size + x0) / self.cols 
-          print(ymin, ymax, xmin, xmax)
-          box_adjusted = np.array([ymin, ymax, xmin, xmax]).astype(np.float32)
-          print("box_adjusted.shape=")
-          print(box_adjusted.shape)
-          print("before concat boxes_all_t.shape=")
-          print(boxes_all_t.shape)
-          boxes_all_t = tf.concat([boxes_all_t, box_adjusted],0)
-          print("after concat boxes_all_t.shape=")
-          print(boxes_all_t.shape)
-          
-        scores_all_t = tf.concat([scores_all_t, scores],1)
-        print("scores.shape=")
-        print(scores.shape)
-        print("scores_all_t.shape=")
-        print(scores_all_t.shape)
-        classes_all_t = tf.concat([classes_all_t, classes],1)
-        print("classes.shape")
-        print(classes.shape)
-        print("classes_all_t.shape")
-        print(classes_all_t.shape)
-        print("num_detections=")
-        print(num_detections)
-        num_detections_all += num_detections
-
-        return boxes_all_t, scores_all_t, classes_all_t, num_detections_all
       
       def get_objects_in_image(self, image_in):
         # Each score represent how level of confidence for each of the objects.
@@ -152,11 +114,6 @@ with detection_graph.as_default():
           patch_id = patch_id + 1
           # the array based representation of the image will be used later in order to prepare the
           # result image with boxes and labels on it.
-          # debug start
-          save_name = '/home/thanuja/test/inputs/patch_%d.png' % (patch_id)
-          print("patch saved!************** i=%d" % (patch_id))
-          cv2.imwrite(save_name, image)
-          # debug stop
           image_np = np.asarray(image)
           # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
           image_np_expanded = np.expand_dims(image_np, axis=0)              
@@ -170,14 +127,6 @@ with detection_graph.as_default():
           num_detections = detection_graph.get_tensor_by_name('num_detections:0')
           (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections], \
               feed_dict={image_tensor: image_np_expanded})
-          
-          '''
-          # put together prediction results into one image representation
-          (boxes_all_t, scores_all_t, classes_all_t, num_detections_all) = self.results_aggregator(\
-                                boxes, scores, classes, num_detections, \
-                                boxes_all_t, scores_all_t, classes_all_t, num_detections_all, \
-                                y0, x0)
-          '''
 
           objects = vis_util.visualize_boxes_and_labels_on_image_array(
               image,
@@ -292,16 +241,12 @@ with detection_graph.as_default():
       def aggregate_patches(self, image_np_in, image_np_array, coords_array):
 
         im_out = image_np_in
-        print("im_out.shape")
-        print(im_out.shape)
         for i in range(len(image_np_array)):
           image_patch = image_np_array[i]
           [start_col, start_row] = coords_array[i]
           start_row = start_row + self.rows / 2
           stop_col = start_col + image_patch.shape[1]
           stop_row = start_row + image_patch.shape[0]
-          print("i, start_col, stop_col, start_row, stop_row")
-          print(i, start_col, stop_col, start_row, stop_row)
           im_out[start_row: stop_row, start_col : stop_col, :] = image_patch[:,:,:]
           
         return im_out
